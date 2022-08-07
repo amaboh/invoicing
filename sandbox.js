c3a53878ae5f3a31c2eabfe0d4f7e8df3e2d5d3a
amaboh <amabocanon@gmail.com>
2022-08-07 13:48:32 +0100

    first commit and project setup

---
+++ sandbox.js
@@ -0,0 +1,5 @@
+var character = 'luigi';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) { return console.log(input); });
---
+++ sandbox.ts
@@ -0,0 +1,8 @@
+var character = 'luigi'
+console.log(character)
+
+var inputs = document.querySelectorAll('input')
+
+console.log(inputs)
+
+inputs.forEach(input => console.log(input))
